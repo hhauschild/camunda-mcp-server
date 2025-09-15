a8582002a822e39158feb6231ec4167beaecda6f
Helmut Hauschild <github@hsec.de>
2025-09-15 15:37:33 +0200

    minor

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,5 @@ Camunda MCP Server - Main package
 """
 
 __version__ = "0.1.0"
-__author__ = "Your Name"
+__author__ = "Helmut Hauschild"
 __description__ = "Model Context Protocol server for Camunda 7.18 integration"
